3b2136800bd63d359b6fe5e3158d849b28c5337f
karthiktanpure1111 <karthiktanpure31@gmail.com>
2025-06-29 18:42:16 +0530

    Add files via upload

---
+++ SudokuSolver/src/com/Sudoku/Solver/SudokuBoard.java
@@ -0,0 +1,57 @@
+package com.Sudoku.Solver;
+
+public class SudokuBoard {
+    private int[][] board;
+
+    public SudokuBoard(int[][] board) {
+        this.board = board;
+    }
+
+    public boolean solve() {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == 0) {
+                    for (int num = 1; num <= 9; num++) {
+                        if (isValid(row, col, num)) {
+                            board[row][col] = num;
+                            if (solve()) return true;
+                            board[row][col] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(int row, int col, int num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num)
+                return false;
+        }
+
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == num)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public void printBoard() {
+        System.out.println("\nSolved Sudoku Grid:");
+        for (int i = 0; i < 9; i++) {
+            if (i % 3 == 0 && i != 0) System.out.println("---------------------");
+            for (int j = 0; j < 9; j++) {
+                if (j % 3 == 0 && j != 0) System.out.print("| ");
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
